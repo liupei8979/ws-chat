626004d7b0d1cf9d93009323a20f6d2fc8c8043c
LimeHayeong <Lime@stamper.network>
2024-01-15 16:56:04 +0900

    chore: backend package installation

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { AuthController } from './auth/auth.controller'
+import { AuthModule } from './auth/auth.module'
+import { ChatModule } from './chat/chat.module'
+import { FirestoreModule } from './firestore/firestore.module'
+import { RedisModule } from './redis/redis.module'
+import { UserModule } from './user/user.module'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [AuthModule, ChatModule, FirestoreModule, RedisModule, UserModule],
+  controllers: [AppController, AuthController],
   providers: [AppService],
 })
 export class AppModule {}
